50aa3d2c98c7b8ee0a2e2917b3e9a2137b0c0dc9
niamba764 <139123500+niamba764@users.noreply.github.com>
2023-10-06 13:04:32 +0200

    Add files via upload

---
+++ exo1.py
@@ -0,0 +1,24 @@
+"""
+We want to have a new class Item such as :
+
+A new item can be created with Item(price, weight)
+    The price and weight of an item can be accessed with
+    item.price and item.weight.
+    Write the code for this class, with the appropriate constructor.
+Example of code using the class: i = Item(10, 20)
+"""
+
+class Item :
+    def __init__(self,prices,weight):
+        self.prices=prices
+        self.weight=weight
+
+    def prices(prices):
+        print(prices)
+    def weight(weight):
+        print(weight)
+
+ 
+        i= Item(10,20)
+
+       
